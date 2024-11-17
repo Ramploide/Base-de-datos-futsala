2b65f55fe46f9918fbf0928f2a9d57cde9cfb091
Ramploide <148322286+Ramploide@users.noreply.github.com>
2024-11-17 18:45:01 -0500

    Add files via upload

---
+++ scripts_act1.sh
@@ -0,0 +1,10 @@
+#! Crear base de datos
+use Futbol
+
+#! Crear colecciones
+db.createCollection("Deportistas")
+db.createCollection("Entrenadores")
+db.createCollection("Equipos")
+db.createCollection("Arbitros")
+db.createCollection("Encuentrosdeportivos")
+db.createCollection("TabladePosiciones")
